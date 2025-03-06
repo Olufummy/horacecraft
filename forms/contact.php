b072b5e9629a32d2ec8878e80202698d788e48fb
Olufummy <adeboyejoisaac18@gmail.com>
2025-03-06 04:08:22 -0800

    js msg

--- forms/contact.php
+++ forms/contact.php
@@ -6,6 +6,7 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
+    if ('Message sent!!!')
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
